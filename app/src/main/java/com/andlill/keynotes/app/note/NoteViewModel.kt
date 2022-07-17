dc5c0a72635c1f1bee01c4893ae1c72629ca13a4
AndreasLill <andreas.ste.lill@gmail.com>
2022-07-17 12:59:09 +0200

    Fixed asynchronous updating note bug.

--- app/src/main/java/com/andlill/keynotes/app/note/NoteViewModel.kt
+++ app/src/main/java/com/andlill/keynotes/app/note/NoteViewModel.kt
@@ -12,8 +12,8 @@ import androidx.lifecycle.viewModelScope
 import com.andlill.keynotes.data.repository.LabelRepository
 import com.andlill.keynotes.data.repository.NoteRepository
 import com.andlill.keynotes.model.Label
+import com.andlill.keynotes.model.Note
 import com.andlill.keynotes.model.NoteLabelJoin
-import com.andlill.keynotes.model.NoteWrapper
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -25,7 +25,7 @@ class NoteViewModel(private val application: Application, private val noteId: In
         override fun <T : ViewModel> create(modelClass: Class<T>): T = NoteViewModel(application, noteId) as T
     }
 
-    private var note = NoteWrapper()
+    private var note = Note()
     private var deleteOnClose = false
 
     var allLabels by mutableStateOf(emptyList<Label>())
@@ -51,7 +51,7 @@ class NoteViewModel(private val application: Application, private val noteId: In
         viewModelScope.launch {
             NoteRepository.getNote(application, noteId).collectLatest {
                 it?.let {
-                    note = it.copy()
+                    note = it.note.copy()
                     noteLabels = it.labels
                     isPinned = it.note.pinned
                     isDeleted = it.note.deleted
@@ -78,26 +78,26 @@ class NoteViewModel(private val application: Application, private val noteId: In
     fun onClose() = viewModelScope.launch {
         // Delete note with null created date.
         if (deleteOnClose) {
-            onCancelReminder()
-            NoteRepository.deleteNote(application, note.note)
+            NoteBroadcaster.cancelReminder(application, note.id)
+            NoteRepository.deleteNote(application, note)
             return@launch
         }
 
         // Delete new note without any modifications.
         if (modifiedDate.isEmpty() && titleText.text.trim().isEmpty() && bodyText.text.trim().isEmpty()) {
-            onCancelReminder()
-            NoteRepository.deleteNote(application, note.note)
+            NoteBroadcaster.cancelReminder(application, note.id)
+            NoteRepository.deleteNote(application, note)
             return@launch
         }
 
         // Don't save note without changes.
-        if (titleText.text.trim() == note.note.title.trim() &&
-            bodyText.text.trim() == note.note.body.trim()) {
+        if (titleText.text.trim() == note.title.trim() &&
+            bodyText.text.trim() == note.body.trim()) {
             return@launch
         }
 
         // Save note.
-        NoteRepository.updateNote(application, note.note.copy(
+        NoteRepository.updateNote(application, note.copy(
             title = titleText.text.trim(),
             body = bodyText.text.trim(),
             modified = Calendar.getInstance().timeInMillis,
@@ -105,17 +105,10 @@ class NoteViewModel(private val application: Application, private val noteId: In
     }
 
     fun onToggleLabel(label: Label) = viewModelScope.launch {
-        if (note.labels.contains(label))
-            NoteRepository.deleteNoteLabel(application, NoteLabelJoin(noteId = note.note.id, labelId = label.id))
+        if (noteLabels.contains(label))
+            NoteRepository.deleteNoteLabel(application, NoteLabelJoin(noteId = note.id, labelId = label.id))
         else
-            NoteRepository.insertNoteLabel(application, NoteLabelJoin(noteId = note.note.id, labelId = label.id))
-    }
-
-    fun onToggleReminder(calendar: Calendar?) {
-        if (calendar != null)
-            onSetReminder(calendar)
-        else
-            onCancelReminder()
+            NoteRepository.insertNoteLabel(application, NoteLabelJoin(noteId = note.id, labelId = label.id))
     }
 
     fun onChangeTitle(value: TextFieldValue) {
@@ -134,30 +127,31 @@ class NoteViewModel(private val application: Application, private val noteId: In
         deleteOnClose = true
     }
 
+    fun onToggleReminder(calendar: Calendar?) = viewModelScope.launch {
+        if (calendar != null) {
+            NoteRepository.updateNote(application, note.copy(reminder = calendar.timeInMillis))
+            NoteBroadcaster.setReminder(application, calendar, note.id)
+        }
+        else {
+            NoteRepository.updateNote(application, note.copy(reminder = null))
+            NoteBroadcaster.cancelReminder(application, note.id)
+        }
+    }
+
     fun onTogglePin() = viewModelScope.launch {
-        NoteRepository.updateNote(application, note.note.copy(pinned = !note.note.pinned))
+        NoteRepository.updateNote(application, note.copy(pinned = !note.pinned))
     }
 
     fun onChangeColor(value: Int) = viewModelScope.launch {
-        NoteRepository.updateNote(application, note.note.copy(color = value))
+        NoteRepository.updateNote(application, note.copy(color = value))
     }
 
     fun onDeleteNote() = viewModelScope.launch {
-        NoteRepository.updateNote(application, note.note.copy(deleted = true))
-        onCancelReminder()
+        NoteRepository.updateNote(application, note.copy(deleted = true, reminder = null))
+        NoteBroadcaster.cancelReminder(application, note.id)
     }
 
     fun onRestore() = viewModelScope.launch {
-        NoteRepository.updateNote(application, note.note.copy(deleted = false))
-    }
-
-    private fun onSetReminder(calendar: Calendar) = viewModelScope.launch {
-        NoteBroadcaster.setReminder(application, calendar, note.note.id)
-        NoteRepository.updateNote(application, note.note.copy(reminder = calendar.timeInMillis))
-    }
-
-    private fun onCancelReminder() = viewModelScope.launch {
-        NoteBroadcaster.cancelReminder(application, note.note.id)
-        NoteRepository.updateNote(application, note.note.copy(reminder = null))
+        NoteRepository.updateNote(application, note.copy(deleted = false))
     }
 }
