22258dc48add90865083af1e5ff1c62309019bf6
AndreasLill <andreas.ste.lill@gmail.com>
2022-08-07 19:16:29 +0200

    Added job scheduler and maintenance job service for automatic deletion of trash notes of 7 days.

--- app/src/main/java/com/andlill/keynotes/app/MainActivity.kt
+++ app/src/main/java/com/andlill/keynotes/app/MainActivity.kt
@@ -2,8 +2,10 @@ package com.andlill.keynotes.app
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -14,15 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Register notification channel for API 26+
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(MainReceiver.CHANNEL_ID, MainReceiver.CHANNEL_ID, NotificationManager.IMPORTANCE_HIGH).apply {
-                enableLights(true)
-                description = MainReceiver.CHANNEL_ID
-            }
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        initNotificationChannel()
+        initJobScheduler()
 
         // Ignore status bar and navigation bar insets to show content behind status bar.
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -33,5 +28,20 @@ class MainActivity : AppCompatActivity() {
                 Navigation()
             }
         }
+    }
+
+    private fun initNotificationChannel() {
+        val channel = NotificationChannel(MainReceiver.CHANNEL_ID, MainReceiver.CHANNEL_ID, NotificationManager.IMPORTANCE_HIGH)
+        channel.enableLights(true)
+        channel.description = MainReceiver.CHANNEL_ID
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun initJobScheduler() {
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val componentName = ComponentName(this, MaintenanceJobService::class.java)
+        val builder = JobInfo.Builder(MaintenanceJobService.SERVICE_ID, componentName).setPersisted(true).setPeriodic(MaintenanceJobService.SERVICE_INTERVAL)
+        jobScheduler.schedule(builder.build())
     }
 }
---
+++ app/src/main/java/com/andlill/keynotes/app/MaintenanceJobService.kt
@@ -0,0 +1,42 @@
+package com.andlill.keynotes.app
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import com.andlill.keynotes.data.repository.NoteRepository
+import com.andlill.keynotes.utils.TimeUtils.daysBetween
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+// Responsible for scheduled background maintenance such as deleting notes scheduled for deletion.
+// Run once every 12 hours.
+class MaintenanceJobService : JobService() {
+
+    companion object {
+        const val SERVICE_ID: Int = 100
+        const val SERVICE_INTERVAL: Long = 12*60*60*1000
+    }
+
+    override fun onStartJob(params: JobParameters): Boolean {
+        deleteScheduledNotes()
+        return false
+    }
+
+    override fun onStopJob(params: JobParameters): Boolean {
+        return false
+    }
+
+    // Permanently delete notes in trash at least 7 days old.
+    private fun deleteScheduledNotes() = CoroutineScope(Dispatchers.Default).launch {
+        NoteRepository.getAllNotes(this@MaintenanceJobService).firstOrNull()?.filter { it.note.deleted }?.forEach { note ->
+            note.note.modified?.let {
+                if (it.daysBetween() <= -7) {
+                    NoteRepository.deleteNote(this@MaintenanceJobService, note.note)
+                    Log.d("MaintenanceJobService", "Scheduled deletion of note: ${note.note.id}")
+                }
+            }
+        }
+    }
+}
