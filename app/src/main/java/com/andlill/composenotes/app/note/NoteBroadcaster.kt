c370c270dfff032dcdc3748fbce788e49f5a0630
AndreasLill <andreas.ste.lill@gmail.com>
2023-03-07 21:36:17 +0100

    Changed alarm type and permission check.

--- app/src/main/java/com/andlill/composenotes/app/note/NoteBroadcaster.kt
+++ app/src/main/java/com/andlill/composenotes/app/note/NoteBroadcaster.kt
@@ -1,10 +1,11 @@
 package com.andlill.composenotes.app.note
 
 import android.app.AlarmManager
+import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.os.Build
 import com.andlill.composenotes.app.MainReceiver
 
 object NoteBroadcaster {
@@ -15,11 +16,23 @@ object NoteBroadcaster {
             intent.action = MainReceiver.ACTION_REMINDER
             intent.putExtra(MainReceiver.EXTRA_NOTE_ID, noteId)
             intent.setPackage("com.andlill.composenotes")
-            PendingIntent.getBroadcast(context, noteId, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getBroadcast(context, noteId, intent, PendingIntent.FLAG_IMMUTABLE)
         }
 
-        alarm.setAlarmClock(AlarmManager.AlarmClockInfo(time, intent), intent)
-        Log.d("NoteBroadcaster", "Broadcast '$noteId' Sent")
+        // TODO: Add permission request.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (alarm.canScheduleExactAlarms()) {
+                // Some devices using API 31 requires permission "SCHEDULE_EXACT_ALARM".
+                alarm.setExactAndAllowWhileIdle(RTC_WAKEUP, time, intent)
+            }
+            else {
+                // Use non-exact as fallback.
+                alarm.setAndAllowWhileIdle(RTC_WAKEUP, time, intent)
+            }
+        }
+        else {
+            alarm.setExactAndAllowWhileIdle(RTC_WAKEUP, time, intent)
+        }
     }
 
     fun cancelReminder(context: Context, noteId: Int) {
@@ -27,10 +40,9 @@ object NoteBroadcaster {
         val intent = Intent(context, MainReceiver::class.java).let { intent ->
             intent.action = MainReceiver.ACTION_REMINDER
             intent.setPackage("com.andlill.composenotes")
-            PendingIntent.getBroadcast(context, noteId, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getBroadcast(context, noteId, intent, PendingIntent.FLAG_IMMUTABLE)
         }
 
         alarm.cancel(intent)
-        Log.d("NoteBroadcaster", "Broadcast '$noteId' Cancelled")
     }
 }
